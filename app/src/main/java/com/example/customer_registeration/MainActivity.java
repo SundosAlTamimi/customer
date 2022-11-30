464e0d714c5394291d3217290c92c70e7d83b8ee
Tahani Ghanoum <tahanighanoum1234@gmail.com>
2022-11-30 16:56:08 +0300

    init

--- app/src/main/java/com/example/customer_registeration/MainActivity.java
+++ app/src/main/java/com/example/customer_registeration/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.customer_registeration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout first_linear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        first_linear=findViewById(R.id.first_linear);
+        first_linear.setOnClickListener(v->{
+            Intent intent=new Intent(this,add_customer.class);
+            startActivity(intent);
+        });
     }
 }
